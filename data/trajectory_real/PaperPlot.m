b2baee14c9923e2332efedf98d37331f3e5b49cf
cghaon <jxiang@ucsd.edu>
2023-04-16 13:22:37 -0700

    paper plot

--- data/trajectory_real/PaperPlot.m
+++ data/trajectory_real/PaperPlot.m
@@ -55,53 +55,6 @@ plot3(historyx(1:hf), historyy(1:hf), historyz(1:hf), 'or:','DisplayName', 'Past
 legend('location', 'Best');
 print(gcf,'Trajectory predicting2','-dpng','-r900');
 %%
-%ld = load("drivingLidarPoint.mat");
-filename = sprintf('realdata2/flightdata%d.csv', 0);
-T = readtable(filename);
-T = table2array(T);
-historyx = [];
-historyy =[];
-historyz =[];
-figure(1)
-i = 1;
-for k = 1:size(T)
-    r = rem( k , 100 )
-    if r ~= 0
-        continue
-    else
-        historyx(i) = T(k, 2);
-        historyy(i) = T(k, 3);
-        historyz(i) = T(k, 4);
-         i = i + 1;
-    end
-end
-hf = floor(length(historyx)/2);
-hf = hf(1);
-%scatter3(historyx, historyy, historyz)
-title("Future Trajectory", 'FontSize', 14)
-xlabel('x', 'FontSize', 14)
-ylabel('y', 'FontSize', 14)
-zlabel('z', 'FontSize', 14)
-grid on
-xlim([-0.1 0.3])
-ylim([-0 1.5])
-zlim([-0.1 2])
-historyx = [];
-historyy =[];
-historyz =[];
-figure(1)
-i = 1;
-for k = 1:size(T)
-    r = rem( k , 100 );
-    if r ~= 0
-        continue
-    else
-        historyx(i) = T(k, 2) + 0.1*(rand() - 0.5);
-        historyy(i) = T(k, 3) + 0.1*(rand() - 0.5);
-        historyz(i) = T(k, 4) + 0.1*(rand() - 0.5);
-        i = i + 1;
-    end
-end
 
 %scatter3(historyx, historyy, historyz)
 % plot3(historyx(hf:length(historyx)), historyy(hf:length(historyx)), historyz(hf:length(historyx)), 'ob:', 'DisplayName', 'Future Trajectory Sensored')
